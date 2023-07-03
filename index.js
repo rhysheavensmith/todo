6b563936de6490f0ac9ad780bee38863fac66abd
Rhys Heaven-Smith <rhysheaven-smith@Rhyss-MacBook-Pro.local>
2023-07-03 14:33:44 +1200

    refactor code

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
+// Store the elements from the dom in variables
 const textInput = document.querySelector(".input");
 const button = document.querySelector(".submit");
 const list = document.querySelector(".list");
 
-function createItem() {
-  if (textInput.value.length > 0) {
-    // Create the li
-    const li = document.createElement("li");
-    li.innerText = textInput.value;
-    li.classList.add("listItem");
-    // Create the delete button
-    const delBtn = document.createElement("button");
-    delBtn.innerText = "delete";
-    delBtn.classList.add("space");
-    // Add the items to the list
-    list.appendChild(li);
-    li.appendChild(delBtn);
-    // Reset the input value to an empty string
-    textInput.value = "";
-    // Add event listeners to the item and the delete button
-    li.addEventListener("click", function () {
-      li.classList.toggle("complete");
-    });
-    delBtn.addEventListener("click", function () {
-      this.parentNode.remove();
-    });
-  }
-}
+// function to create list item
+const createItem = () => {
+  const li = document.createElement("li");
+  li.innerText = textInput.value;
+  li.classList.add("listItem");
+  li.addEventListener("click", function () {
+    li.classList.toggle("complete");
+  });
+  list.appendChild(li);
+  const delBtn = createDelete();
+  li.appendChild(delBtn);
+};
 
-button.addEventListener("click", createItem);
+// function to create a delete button
+const createDelete = () => {
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "delete";
+  delBtn.classList.add("space");
+  delBtn.addEventListener("click", function () {
+    this.parentNode.remove();
+  });
+  return delBtn;
+};
+
+// function to add the to do to the list
+const addToDo = () => {
+  const trimValue = textInput.value.trim();
+  if (trimValue.length > 0) {
+    createItem();
+    textInput.value = "";
+  }
+};
+
+// Add event listeners to the button and the enter key
+button.addEventListener("click", addToDo);
 
 textInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter" || e.keyCode === 13) {
-    createItem();
+    addToDo();
   }
 });
